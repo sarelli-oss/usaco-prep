7e011ddc668ae0c0096e76b87f24b05e79fb9177
sarelli-oss <cherry.arelli@gmail.com>
2026-08-04 19:30:45 -0500

    Weird Algorithm completed(CSES Introductory Problem)

---
+++ usaco_guide/weird_algorithm.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+void solve() {
+
+    long long n;
+    cin >> n;
+
+    std::vector<long long> all_ns;
+    all_ns.push_back(n);    
+
+    while(n != 1) {
+
+        if(n % 2 == 0) {
+            n /= 2;
+        }
+
+        else {
+            n = 3*n + 1;
+        }
+
+        all_ns.push_back(n);
+
+
+        
+
+        
+    }
+
+    
+
+    for (int x : all_ns) {
+    cout << x << " ";
+}
+    cout << "\n";   
+    
+
+}
+
+int main() {
+    //os_base::sync_with_stdio(false);
+    //cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
